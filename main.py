b0a8bc67cc0b394c3b8d984adb23008e2129440b
kjonathan024 <63828254+kjonathan024@users.noreply.github.com>
2021-06-26 10:30:52 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,66 +1,78 @@
+import traceback
 import os
 import sys
 from PIL import Image, ExifTags
 from docx import Document
 from docx.shared import Inches
 from tkinter import Tk, filedialog
-root = Tk() # pointing root to Tk() to use it as Tk() in program.
-root.withdraw() # Hides small tkinter window.
 
-root.attributes('-topmost', True) # Opened windows will be active. above all windows despite of selection.
+try:
+    root = Tk()  # pointing root to Tk() to use it as Tk() in program.
+    root.withdraw()  # Hides small tkinter window.
 
-path = filedialog.askdirectory() # Returns opened path as str
+    root.attributes('-topmost', True)  # Opened windows will be active. above all windows despite of selection.
 
-def renameWithDate(exif, file):
-    date = exif.get("DateTimeOriginal")
-    date = date[0:date.find(' ')]
-    dateNew = date.replace(':', '-')
-    newFileName = f'{file[0:file.find(".")]} {dateNew}{file[file.find("."):]}'
-    if dateNew in file:
-        return file
-    else:
-        return newFileName
+    path = filedialog.askdirectory()  # Returns opened path as str
 
-for file in os.listdir(path):
-    print(file)
-    if file != ".DS_Store" and '.docx' not in file: #may want to add to check if they are jpeg, JPG, PNG, etc
-        img = Image.open(f'{path}/{file}')
-        exif = {ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS}
-        exif.pop('MakerNote', '')
-        ''' prints the dictionary
-        for k, v in exif.items():
-            print(f'{k}: {v}')
-        print()
-        '''
-        document = Document()
-        document.add_picture(f'{path}/{file}', width = Inches(6.00))
-        table = document.add_table(rows=1, cols=2)
-        hdr_cells = table.rows[0].cells
-        hdr_cells[0].text = 'Key'
-        hdr_cells[1].text = 'Value'
-        for k, v in exif.items():
-            row_cells = table.add_row().cells
-            row_cells[0].text =  f'{k}'
-            row_cells[1].text = f'{v}'
+    desiredExifs = ['GPSInfo', 'Make', 'Model', 'DateTime', 'XResolution', 'YResolution', 'DateTimeOriginal', 'ExifImageWidth', 'ExifImageHeight']
 
-        if exif.get("DateTimeOriginal") is None:
-            print(f'{file} has no date metadata.\n')
+    def renameWithDate(exif, file):
+        date = exif.get("DateTimeOriginal")
+        date = date[0:date.find(' ')]
+        dateNew = date.replace(':', '-')
+        newFileName = f'{file[0:file.find(".")]} {dateNew}{file[file.find("."):]}'
+        if dateNew in file:
+            return file
         else:
-            action = renameWithDate(exif, file)
-            os.rename(f'{path}/{file}', f'{path}/{action}')
-        document.save(f'{path}/Metadata {action[0:action.find(".")]}.docx')
-        #still don't understand dictionaries
-        '''
-        if exif.get("DateTimeOriginal") is None:
-            print(f'{file} has no date metadata.')
-        else:
-            date = exif.get("DateTimeOriginal")
-            date = date[0:date.find(' ')]
-            dateNew = date.replace(':', '-')
-            newFileName = f'{file[0:file.find(".")]} {dateNew}{file[file.find("."):]}'
-            print(newFileName)
-            if dateNew in file:
-                pass
+            return newFileName
+
+
+    for file in os.listdir(path):
+        print(file)
+        if (file != ".DS_Store" and '.docx' not in file) and ('.JPG' in file or '.jpeg' in file or '.jpg' in file or '.JPEG' in file):  # may want to add to check if they are jpeg, JPG, PNG, etc
+            img = Image.open(f'{path}/{file}')
+            exif = {ExifTags.TAGS[k]: v for k, v in img._getexif().items() if k in ExifTags.TAGS}
+            
+            ''' prints the dictionary
+            for k, v in exif.items():
+                print(f'{k}: {v}')
+            print()
+            '''
+            document = Document()
+            document.add_picture(f'{path}/{file}', width=Inches(6.00))
+            table = document.add_table(rows=1, cols=2)
+            hdr_cells = table.rows[0].cells
+            hdr_cells[0].text = 'Key'
+            hdr_cells[1].text = 'Value'
+            for k, v in exif.items():
+                row_cells = table.add_row().cells
+                row_cells[0].text = f'{k}'
+                row_cells[1].text = f'{v}'
+
+            if exif.get("DateTimeOriginal") is None:
+                print(f'{file} has no date metadata.\n')
             else:
-                os.rename(f'{path}/{file}', f'{path}/{newFileName}')
-        '''
+                action = renameWithDate(exif, file)
+                img.close()
+                os.rename(f'{path}/{file}', f'{path}/{action}')
+            document.save(f'{path}/Metadata {action[0:action.find(".")]}.docx')
+            # still don't understand dictionaries
+            '''
+            if exif.get("DateTimeOriginal") is None:
+                print(f'{file} has no date metadata.')
+            else:
+                date = exif.get("DateTimeOriginal")
+                date = date[0:date.find(' ')]
+                dateNew = date.replace(':', '-')
+                newFileName = f'{file[0:file.find(".")]} {dateNew}{file[file.find("."):]}'
+                print(newFileName)
+                if dateNew in file:
+                    pass
+                else:
+                    os.rename(f'{path}/{file}', f'{path}/{newFileName}')
+            '''
+except Exception:
+    print(traceback.format_exc())
+    input("Press return to exit")
+
+#information to include: GPSInfo, Make, Model, DateTime, XResolution, YResolution, DateTimeOriginal, ExifImageWidth, ExifImageHeight
